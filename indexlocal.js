e4dc86e519a77c1bcf6d2bf95409c25385616b49
Ronald Britos <rmbgkpo@ensign.edu>
2024-04-03 10:17:34 -0600

    new changes new build

--- indexlocal.js
+++ indexlocal.js
@@ -1,3 +1,49 @@
+const express = require("express");
+const Redis = require("redis");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const { addOrder, getOrder } = require("./services/orderservice.js");
+const { addOrderItem, getOrderItem } = require("./services/orderItems.js");
+const fs = require("fs");
+const Schema = JSON.parse(fs.readFileSync("./orderItemSchema.json", "utf8"));
+const Ajv = require("ajv");
+const ajv = new Ajv();
+const awsServerlessExpress = require("aws-serverless-express");
+
+// Setup
+const app = express();
+app.use(bodyParser.json());
+app.use(cors({ origin: "http://localhost:3000" }));
+
+// Redis client setup
+const redisClient = Redis.createClient({
+  url: `redis://localhost:6379`,
+});
+
+// Connect to Redis
+redisClient.connect().catch(console.error);
+
+// Define routes as usual
+app.post("/orders", async (req, res) => {
+  /* ... */
+});
+app.get("/orders/:orderId", async (req, res) => {
+  /* ... */
+});
+app.post("/orderItems", async (req, res) => {
+  /* ... */
+});
+app.get("/orderItems/:orderItemId", async (req, res) => {
+  /* ... */
+});
+
+// No need to listen on a port, AWS Lambda will handle incoming requests
+const server = awsServerlessExpress.createServer(app);
+
+exports.handler = (event, context) => {
+  awsServerlessExpress.proxy(server, event, context);
+};
+
 // const express = require("express"); // express makes APIs - connect frotend to database
 // const Redis = require("redis"); //redis is a database, import the Redis class from the redis library
 // const bodyParser = require("body-parser"); //body-parser is a library that allows us to read the body of a request
